9e43c69392d8014bca85bee07ed6e7b2d23c4200
Jakub Piecha <piechajakub@wp.pl>
2023-09-11 09:31:33 +0200

    change database schema for one to many

--- backend/conftest.py
+++ backend/conftest.py
@@ -9,9 +9,9 @@ from currency.models import CurrencyName, CurrencyDate, CurrencyValue
 def currency(db):
     currency_name = CurrencyName.objects.get(code="EUR")
     currency_date = CurrencyDate.objects.create(date=date.today())
-    currency_value = CurrencyValue.objects.create(exchange_rate=5.50)
-    currency_value.currency_names.add(currency_name)
-    currency_value.currency_dates.add(currency_date)
+    currency_value = CurrencyValue.objects.create(
+        exchange_rate=5.50, currency_name=currency_name, currency_date=currency_date
+    )
     return currency_date
 
 
@@ -19,7 +19,19 @@ def currency(db):
 def currency_euro(db):
     currency_name = CurrencyName.objects.get(code="EUR")
     currency_date = CurrencyDate.objects.create(date=date.today() - timedelta(days=3))
-    currency_value = CurrencyValue.objects.create(exchange_rate=3.50)
-    currency_value.currency_names.add(currency_name)
-    currency_value.currency_dates.add(currency_date)
+    currency_value = CurrencyValue.objects.create(
+        exchange_rate=3.50, currency_name=currency_name, currency_date=currency_date
+    )
     return currency_date
+
+
+@pytest.fixture
+def currency_date(db):
+    currency_date = CurrencyDate.objects.create(date=date(2023, 9, 11))
+    return currency_date
+
+
+@pytest.fixture
+def currency_name(db):
+    currency_name = CurrencyName.objects.get(code="SEK")
+    return currency_name
--- backend/currency/tests/test_models.py
+++ backend/currency/tests/test_models.py
@@ -1,21 +1,31 @@
+import pytest
 from _decimal import Decimal
 from datetime import date, timedelta
+
+from django.db import IntegrityError
 
 from conftest import currency
 from currency.models import CurrencyValue, CurrencyDate
 
 
 def test_add_one_currency_data(currency):
-    currency_value = CurrencyValue.objects.filter(currency_dates=currency).first()
+    currency_value = CurrencyValue.objects.filter(currency_date=currency).first()
 
-    assert currency_value.currency_dates.first().date == currency.date
-    assert currency_value.currency_names.first().code == "EUR"
-    assert currency_value.currency_names.first().name == "euro"
+    assert currency_value.currency_date.date == currency.date
+    assert currency_value.currency_name.code == "EUR"
+    assert currency_value.currency_name.name == "euro"
 
 
 def test_get_value_by_date_range(currency, currency_euro):
     dates = CurrencyDate.objects.filter(date__range=(date.today() - timedelta(days=5), date.today()))
-    values = dates.filter(currency_values__currency_names__code="EUR")
-
+    values = dates.filter(currency_values__currency_name__code="EUR")
     assert len(values) == 2
     assert values.last().currency_values.first().exchange_rate == Decimal("3.50")
+
+
+def test_add_two_value_for_one_date_and_currency(currency_date, currency_name):
+    CurrencyValue.objects.create(exchange_rate=3.47, currency_date=currency_date, currency_name=currency_name)
+
+    with pytest.raises(IntegrityError) as excinfo:
+        CurrencyValue.objects.create(exchange_rate=3.33, currency_date=currency_date, currency_name=currency_name)
+    assert "UNIQUE constraint failed" in str(excinfo)
